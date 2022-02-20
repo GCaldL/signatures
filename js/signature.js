64c86a6a889e06c2d14674b28b7bd67ffa296f21
GCaldL <geoff.c.777@gmail.com>
2022-02-21 09:02:30 +1000

    more clean up

--- js/signature.js
+++ js/signature.js
@@ -1,11 +1,12 @@
-
-
 function generateTemplate(name, role, wp, mp, em, site, address) {
     
 }
 
-
 console.log("test")
+
+$.get('templates/layout.html', function(data) {
+    console.log(data);
+}, 'text');
 
 //I can't be effed checking specific el for update, just update the freaking thing regardless :)
 $("*").on('change keyup paste', function () {
